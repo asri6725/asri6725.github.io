7aa926f0937c52a88570b03cee4309ac767d1827
Abhi <abhinandan.srinivas@gmail.com>
2020-11-25 13:13:07 +1100

    Minor Changes, fixing resume button and styles

--- src/About.js
+++ src/About.js
@@ -1,33 +1,34 @@
 import profile from './files/profile.png';
 import './About.css';
-import Resume from './files/Abhinandan Resume.pdf';
+import Resume from './files/Abhinandan Srivinas Resume.pdf';
 function About(){
     return(
         <div className="AboutWrap">
-            
+            <div>
+                    <p className="AboutHeading"> About </p>
+                </div>
         
             <div className="Graphic">
                 <img src={profile} alt="profile" className="AboutImage" />
 
                 <br />
                 <br />
+                <br />
                 <div className="buttonpos">
                     <a className="SecondaryButton" href="https://github.com/asri6725/" rel="noopener noreferrer" target="_blank"> GitHub </a>
-                    <a className="SecondaryButton" href={Resume} rel="noopener noreferrer" target="_blank"> Resume </a>
+                    <a className="SecondaryButton" href={Resume} rel="noopener noreferrer" target="_blank" download> Resume </a>
                 </div>    
             </div>
 
             <div className="AboutText">
-                <div>
-                    <p className="AboutHeading"> About </p>
-                </div>
+                
 
                 <div>
                     <p className="text">
-                    I am a recent graduate who is looking to break into development.
-                    I am good with the full stack of web development, primarily working with React, Python and an SQL database.
-                    <br /><br />
-                    I hold a Bachelors of Computer Science and IT (Comp Sci major) from the University of Sydney and well, thats about all about me.
+                    I am a Computer Science and IT Graduate with experience developing front end software and applications, collaborating with design, back-end and product teams, utilising Python, React, HTML, CSS, JavaScript, SQL, and more in the health, SaaS and education sectors. I have developed and designed a billing service interface for a client at Saemushi, working with AngularJS, ensuring high-quality UI and UX outcomes. 
+<br /><br />
+I am passionate about collaborating with multi-disciplinary teams to deliver high-quality and innovative value-add solutions. I am looking for an opportunity to utilise my skills in a new Developer role.
+
                     </p>
                 </div>
                 
