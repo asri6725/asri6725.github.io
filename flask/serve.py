5549b94b7ffe38b21cf7ad4bbc6c0dbb822ecff1
EC2 Default User <ec2-user@ip-172-31-36-93.us-east-2.compute.internal>
2020-12-03 04:27:09 +0000

    Server side code changes

--- flask/serve.py
+++ flask/serve.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
-
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-
+    return app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=False)
+    app.run(host="0.0.0.0",port=80)
